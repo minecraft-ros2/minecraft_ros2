fbcd038416848ba8c057e3c44bedf121e2cb6eb6
Masaaki Hijikata <hijimasa@gmail.com>
2025-05-22 08:28:26 +0900

    [change] Enable GPU automatically

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,6 @@
 xhost +local:root
-docker run -it --rm --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" minecraft_ros2:latest 
+if type nvidia-container-runtime >/dev/null 2>&1; then
+  GPU_OPT="--gpus all"
+fi
+docker run -it --rm ${GPU_OPT} --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" minecraft_ros2:latest 
 xhost -local:root
